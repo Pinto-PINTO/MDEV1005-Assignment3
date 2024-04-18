f982fed33548f3964d566be59d51a5e76e2790ad
Pinto-PINTO <marie334fe@gmail.com>
2024-04-17 21:32:26 -0400

    USA Population API data fetch completed

--- lib/components/population_api.dart
+++ lib/components/population_api.dart
@@ -1,15 +1,35 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'package:mdev1005_flutter_assign3/screens/welcome_screen.dart';
-import '../routes/routes.dart';
+import 'package:http/http.dart' as http;
 import '../widgets/custom_scaffold.dart';
 
 class PopulationApiScreen extends StatefulWidget {
-
   @override
   State<PopulationApiScreen> createState() => _PopulationApiScreenState();
 }
 
 class _PopulationApiScreenState extends State<PopulationApiScreen> {
+  List<Map<String, dynamic>> populationData = [];
+
+  @override
+  void initState() {
+    super.initState();
+    fetchData();
+  }
+
+  Future<void> fetchData() async {
+    final response = await http.get(Uri.parse(
+        'https://datausa.io/api/data?drilldowns=Nation&measures=Population'));
+    if (response.statusCode == 200) {
+      final jsonData = json.decode(response.body);
+      setState(() {
+        populationData = List<Map<String, dynamic>>.from(jsonData['data']);
+      });
+    } else {
+      throw Exception('Failed to load data');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return CustomScaffold(
@@ -62,7 +82,7 @@ class _PopulationApiScreenState extends State<PopulationApiScreen> {
                             horizontal: 10,
                           ),
                           child: Text(
-                            'Fetching data from datausa.io API',
+                            'Displays Population data from 2021 to 2013',
                             style: TextStyle(
                               color: Colors.black45,
                             ),
@@ -79,7 +99,43 @@ class _PopulationApiScreenState extends State<PopulationApiScreen> {
                     const SizedBox(
                       height: 30.0,
                     ),
-
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: populationData
+                          .map((data) => ClipRRect(
+                        borderRadius: BorderRadius.circular(8),
+                        child: Card(
+                          elevation: 4,
+                          margin: const EdgeInsets.all(10),
+                          child: Container(
+                            color: Color(0xFF4A5981), // Background color
+                            padding: const EdgeInsets.all(20),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  'Year: ${data['Year']}',
+                                  style: TextStyle(
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white, // Text color
+                                  ),
+                                ),
+                                const SizedBox(height: 10),
+                                Text(
+                                  'Population: ${data['Population']}',
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                    color: Colors.white, // Text color
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ))
+                          .toList(),
+                    ),
 
                   ],
                 ),
