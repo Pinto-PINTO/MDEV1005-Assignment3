76d52c4413df0c670d2efa34d54c11de72d8ac01
Pinto-PINTO <marie334fe@gmail.com>
2024-04-19 11:07:39 -0400

    Created Calculator Logic

--- lib/components/calculator.dart
+++ lib/components/calculator.dart
@@ -1,15 +1,95 @@
 import 'package:flutter/material.dart';
-import 'package:mdev1005_flutter_assign3/screens/welcome_screen.dart';
 import '../routes/routes.dart';
 import '../widgets/custom_scaffold.dart';
 
 class CalculatorScreen extends StatefulWidget {
-
   @override
   State<CalculatorScreen> createState() => _CalculatorScreenState();
 }
 
 class _CalculatorScreenState extends State<CalculatorScreen> {
+  String output = "0";
+  String _output = "0";
+  double num1 = 0.0;
+  double num2 = 0.0;
+  String operand = "";
+
+  buttonPressed(String buttonText) {
+    if (buttonText == "CLEAR") {
+      _output = "0";
+      num1 = 0.0;
+      num2 = 0.0;
+      operand = "";
+    } else if (buttonText == "+" || buttonText == "-" || buttonText == "*" || buttonText == "/") {
+      num1 = double.parse(output);
+      operand = buttonText;
+      _output = "0";
+    } else if (buttonText == ".") {
+      if (_output.contains(".")) {
+        return;
+      } else {
+        _output = _output + buttonText;
+      }
+    } else if (buttonText == "=") {
+      num2 = double.parse(output);
+      if (operand == "+") {
+        _output = (num1 + num2).toString();
+      }
+      if (operand == "-") {
+        _output = (num1 - num2).toString();
+      }
+      if (operand == "*") {
+        _output = (num1 * num2).toString();
+      }
+      if (operand == "/") {
+        _output = (num1 / num2).toString();
+      }
+      num1 = 0.0;
+      num2 = 0.0;
+      operand = "";
+    } else {
+      _output = _output + buttonText;
+    }
+
+    setState(() {
+      output = double.parse(_output).toStringAsFixed(2);
+    });
+  }
+
+  Widget buildButton(String buttonText) {
+    Color textColor = Color(0xFF6C84C9);
+    Color buttonColor = Colors.white;
+
+    // Check if the button is "="
+    if (buttonText == "=") {
+      textColor = Colors.white;
+      buttonColor = Color(0xFF6C84C9);
+    }
+
+    return Expanded(
+      child: OutlinedButton(
+        onPressed: () => buttonPressed(buttonText),
+        style: ButtonStyle(
+          shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+            RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(0.0),
+            ),
+          ),
+          backgroundColor: MaterialStateProperty.all<Color>(buttonColor),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(24.0),
+          child: Text(
+            buttonText,
+            style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold, color: textColor),
+          ),
+        ),
+      ),
+    );
+  }
+
+
+
   @override
   Widget build(BuildContext context) {
     return CustomScaffold(
@@ -77,14 +157,61 @@ class _CalculatorScreenState extends State<CalculatorScreen> {
                       ],
                     ),
                     const SizedBox(
+                      height: 10.0,
+                    ),
+                    Text(
+                      output,
+                      style: TextStyle(
+                        fontSize: 24.0,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black,
+                      ),
+                    ),
+                    const SizedBox(
                       height: 30.0,
                     ),
-
-
-                    // --------- Calculator Logic ---------
-
-
-
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        buildButton("7"),
+                        buildButton("8"),
+                        buildButton("9"),
+                        buildButton("/")
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        buildButton("4"),
+                        buildButton("5"),
+                        buildButton("6"),
+                        buildButton("*")
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        buildButton("1"),
+                        buildButton("2"),
+                        buildButton("3"),
+                        buildButton("-")
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        buildButton("."),
+                        buildButton("0"),
+                        buildButton("+")
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        buildButton("CLEAR"),
+                        buildButton("="),
+                      ],
+                    ),
                   ],
                 ),
               ),
